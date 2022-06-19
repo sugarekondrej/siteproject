0f2b1907cfae8d17503cbe0f903e5a86eed8e375
sugarekondrej <ondrej.sugarek@gmail.com>
2022-06-20 00:42:51 +0100

    Adding search function to listings

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,10 @@
+from ast import keyword
 from django.shortcuts import render
 from . import models
 from listings import urls
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from listings.choices import state_choices,bedroom_choices,price_choices
+
 # Create your views here.
 def index(request):
     listings = models.Listings.objects.all()
@@ -10,7 +13,8 @@ def index(request):
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
     context = {
-            'listings': paged_listings
+            'listings': paged_listings,
+           
 
     }
     return render(request,'listings/listings.html',context)
@@ -25,4 +29,43 @@ def listing(request,listing_id):
     return render(request,'listings/listing.html',context)
 
 def search(request):
-    return render(request,'listings/search.html')
+    query_list = models.Listings.objects.order_by('-list_date')
+
+    #keyword
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_list = query_list.filter(description__icontains=keywords)
+    #city
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_list = query_list.filter(city__iexact=city)
+    #state
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_list = query_list.filter(state__iexact=state)
+    #bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_list = query_list.filter(bedrooms__iexact=bedrooms)
+    #price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query_list = query_list.filter(price__lte=price)
+
+    
+    context = {
+        "bedroom_choices": bedroom_choices,
+        "price_choices":price_choices,
+        "state_choices": state_choices,
+        "listings": query_list
+        }
+
+
+    
+
+    return render(request,'listings/search.html',context)
--- pages/views.py
+++ pages/views.py
@@ -2,11 +2,16 @@ from multiprocessing import context
 from urllib import request
 from django.shortcuts import render
 from listings import models
+from listings.choices import state_choices,bedroom_choices,price_choices
 # Create your views here.
 def index(request):
     listings = models.Listings.objects.all()
     context = {
-        "listings":listings
+        "listings":listings,
+        "bedroom_choices": bedroom_choices,
+        "price_choices":price_choices,
+        "state_choices": state_choices
+
     }
 
     return render(request,'pages/index.html',context)
